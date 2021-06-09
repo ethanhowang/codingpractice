fa2a723ce6d51ea09c7720cf47858492cdd132ae
Ethan Wang <ethan_wang@berkeley.edu>
2021-06-08 22:19:41 -0700

    wordsearch

---
+++ wordsearch.java
@@ -0,0 +1,23 @@
+class Solution {
+    public findneighbors(char[][], int row, int col) {
+      int rows[]
+    }
+    public boolean exist(char[][] board, String word) {
+        if  (word == "") {
+          return true;
+        }
+        int m = board.length;
+        int n = board[0].length;
+
+        for (int i=0; i < m; i++) {
+            for (int j=0; j < n; j++) {
+                if (board[i][j] == word.charAt(0)) {
+
+                }
+            }
+        }
+        return false;
+    }
+}
+
+Solution.exist()
